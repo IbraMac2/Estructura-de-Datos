19b4197f170d6bcf12ccfa76fb12a6f6bc4dcb48
IbraMac2 <158487512+IbraMac2@users.noreply.github.com>
2024-02-08 11:31:40 -0600

    Add files via upload

---
+++ MemoriaEstatica.py
@@ -0,0 +1,9 @@
+# Crear un arreglo de calificaciones con 5 elementos
+calificaciones = [0] * 5
+
+# Utilizar un bucle para capturar las calificaciones
+for i in range(5):
+    calificaciones[i] = int(input("Capture la calificación: "))
+
+# Imprimir las calificaciones
+print("Calificaciones ingresadas:", calificaciones)
