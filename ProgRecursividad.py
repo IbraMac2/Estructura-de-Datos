1ada77988450bea995640e01dc00c753133a33df
IbraMac2 <158487512+IbraMac2@users.noreply.github.com>
2024-02-27 17:58:25 -0600

    PRACT4 - ProgRecursividad

---
+++ ProgRecursividad.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    
+    if n == 0:
+        return 1
+   
+    else:
+        return n * factorial(n - 1)
+
+numero = 10
+resultado = factorial(numero)
+print(f"El factorial de {numero} es {resultado}")
