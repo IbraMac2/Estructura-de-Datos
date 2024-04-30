96415ddaca084ea91772b105276408beca83ace8
IbraMac2 <158487512+IbraMac2@users.noreply.github.com>
2024-04-29 20:05:34 -0600

    Add files via upload

---
+++ Unidad5y6/MetodosOrdenamiento.py
@@ -0,0 +1,35 @@
+import random
+
+random_list = [random.randint(1, 100) for _ in range(1000)]
+print("Lista original:", random_list)
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+print("Ordenado por método de burbuja:", bubble_sort(random_list.copy()))
+print("Ordenado por método de inserción:", insertion_sort(random_list.copy()))
+print("Ordenado por método de selección:", selection_sort(random_list.copy()))
